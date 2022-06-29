a0bf5bc04e3efe0053769a56793258846dea263f
Susan Minott <susan_minott@yahoo.com>
2022-06-29 08:43:30 -0400

    commit tree bec188d940f875ec814b33c20068041ff6cd9ed1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: :true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: 'Fiction'..'Non-fiction'}
+    validate :allow_value
+
+    def allow_value
+        unless title.include("Won't Believe"||"Secret"||"Top 5"||"Guess")
+            render json: { errors: title.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
